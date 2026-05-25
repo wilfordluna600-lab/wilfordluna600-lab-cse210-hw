39882191d7b1ac70d84038d432dc27b02b8763cd
wilfordluna600-lab <wilfordluna600@gmail.com>
2026-05-25 17:08:35 -0500

    changes

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,24 @@
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction firstFraction = new Fraction();
+        Console.WriteLine($"{firstFraction._top}/{firstFraction._bottom}");
+
+        Fraction secondFraction = new Fraction(5);
+        Console.WriteLine($"{secondFraction._top}/{secondFraction._bottom}");
+
+        Fraction thirdFraction = new Fraction(6, 7);
+        Console.WriteLine($"{thirdFraction._top}/{thirdFraction._bottom}");
+
     }
 }

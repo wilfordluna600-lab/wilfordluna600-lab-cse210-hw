4058eb9dd341880983d4b0b2e44eff8f5e66a50d
wilfordluna600-lab <wilfordluna600@gmail.com>
2026-05-25 18:56:32 -0500

    changes

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -7,17 +7,16 @@ class Program
         Console.WriteLine("Hello World! This is the Fractions Project.");
 
         Fraction firstFraction = new Fraction();
-        Console.WriteLine($"{firstFraction.GetTop()}/{firstFraction.GetBottom()}");
 
         Fraction secondFraction = new Fraction();
         secondFraction.SetTop(5);
-        Console.WriteLine($"{secondFraction.GetTop()}/{secondFraction.GetBottom()}");
 
         Fraction thirdFraction = new Fraction();
         thirdFraction.SetTop(6);
         thirdFraction.SetBottom(7);
+
+        Console.WriteLine($"{firstFraction.GetTop()}/{firstFraction.GetBottom()}");
+        Console.WriteLine($"{secondFraction.GetTop()}/{secondFraction.GetBottom()}");
         Console.WriteLine($"{thirdFraction.GetTop()}/{thirdFraction.GetBottom()}");
-
-
     }
 }

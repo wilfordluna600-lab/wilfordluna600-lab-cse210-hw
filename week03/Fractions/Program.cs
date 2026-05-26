b1e45a3cbaaeb5328388adccc44fe4ad4fc901f1
wilfordluna600-lab <wilfordluna600@gmail.com>
2026-05-26 18:11:14 -0500

    changes

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -15,8 +15,15 @@ class Program
         f3.SetTop(6);
         f3.SetBottom(7);
 
-        Console.WriteLine($"{f1.GetTop()}/{f1.GetBottom()}");
-        Console.WriteLine($"{f2.GetTop()}/{f2.GetBottom()}");
-        Console.WriteLine($"{f3.GetTop()}/{f3.GetBottom()}");
+        Fraction f4 = new Fraction();
+        f4.SetTop(1);
+        f4.SetBottom(3);
+
+        // Console.WriteLine($"{f1.GetTop()}/{f1.GetBottom()}");
+        // Console.WriteLine($"{f2.GetTop()}/{f2.GetBottom()}");
+        // Console.WriteLine($"{f3.GetTop()}/{f3.GetBottom()}");
+
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
     }
 }

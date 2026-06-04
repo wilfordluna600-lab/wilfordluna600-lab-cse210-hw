5e303a005b0fa4b10f329cfeecf267c88743ea7d
wilfordluna600-lab <wilfordluna600@gmail.com>
2026-06-04 14:20:29 -0500

    changes

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -6,5 +6,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+
+
     }
 }
--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -7,16 +7,23 @@ public class Video
     // I am still thinking If I'm going to use 
     // this comments variable or not   
 
-    public Video(string title, string author, int length)
+    public Video(string title, string author, int length, List<Comment> comments)
     {
         _title = title;
         _author = author;
         _length = length;
         _comments = new List<Comment>();
+        _comments = comments;
     }
 
     public int GetNumberOfComments()
     {
         return _comments.Count;
     }
+
+    public string GetFullInfoVideo()
+    {
+        return $"Title: {_title} Author: {_author} Length: {_length} seconds\nNumber of Comments: {GetNumberOfComments()}";
+    }
+
 }

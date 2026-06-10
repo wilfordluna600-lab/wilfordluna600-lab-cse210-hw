7e0342b2db12fe08a9706444bc07db1cd8011f1b
wilfordluna600-lab <wilfordluna600@gmail.com>
2026-06-09 23:06:28 -0500

    changes

--- week05/Mindfulness/ReflectingActivity.cs
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -3,5 +3,35 @@ public class ReflectingActivity : Activity
     public List<string> _prompts;
     public List<string> _questions;
 
+    public ReflectingActivity()
+    {
+        // It's very possible that I'm going to change this part later,
+        // I'm going to check it more later
+        _prompts = new List<string>();
+    }
 
+    public void Run()
+    {
+
+    }
+
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+
+    public string GetRandomQuestion()
+    {
+        return "";
+    }
+
+    public void DisplayPrompt()
+    {
+
+    }
+
+    public void DisplayQuestions()
+    {
+
+    }
 }

500cf59abbc7fef3ee83d0c9625650c74e63ad1e
wilfordluna600-lab <wilfordluna600@gmail.com>
2026-06-11 15:56:50 -0500

    changes

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -1,12 +1,27 @@
 public class BreathingActivity : Activity
 {
-    public BreathingActivity()
+    public BreathingActivity(int duration) : base(duration)
     {
-
+        ;
     }
 
     public void Run()
     {
+
+        // Index this part I am writing some code first and then
+        // I am going to review that it work wery well.
+
+        DateTime startTime = new DateTime.Now;
+        DateTime endTime = startTime.Add(duration);
+        Console.WriteLine();
+        Console.Write("Breathe in...");
+        ShowCountDown(2);
+        Console.Write("Now breathe out...");
+        ShowCountDown(3);
+        Console.WriteLine();
+
+
+
 
     }
 }

51c770dc9c6eec5bf832ccd012542afccd11655f
wilfordluna600-lab <wilfordluna600@gmail.com>
2026-06-11 12:18:17 -0500

    changes

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -33,12 +33,43 @@ public class Activity
 
     public void DisplayEndingMessage()
     {
-
+        Console.WriteLine("Well done!!");
+        ShowSpinner(5);
+        Console.WriteLine($"You have completed another {_duration} seconds of the {_name}.");
+        ShowSpinner(5);
     }
 
     public void ShowSpinner(int seconds)
     {
+        List<string> animationStrings = new List<string>();
+        animationStrings.Add("|");
+        animationStrings.Add("/");
+        animationStrings.Add("-");
+        animationStrings.Add("\\");
+        animationStrings.Add("|");
+        animationStrings.Add("/");
+        animationStrings.Add("-");
+        animationStrings.Add("\\");
 
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(seconds);
+
+        int i = 0;
+
+        while (DateTime.Now < endTime)
+        {
+            string s = animationStrings[i];
+            Console.Write(s);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+
+            i++;
+
+            if (i >= animationStrings.Count)
+            {
+                i = 0;
+            }
+        }
     }
 
     public void ShowCountDown(int seconds)

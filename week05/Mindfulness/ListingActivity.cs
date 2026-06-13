1c07a2f2b6e09548e5cb9d4ef861e1f0001bf90a
wilfordluna600-lab <wilfordluna600@gmail.com>
2026-06-13 00:26:25 -0500

    changes

--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -1,28 +1,76 @@
-// public class ListingActivity : Activity
-// {
-//     private int _count;
-//     private List<string> _prompts;
+using System;
+using System.Collections.Generic;
+public class ListingActivity : Activity
+{
+    private int _count;
+    private List<string> _prompts = new List<string>()
+    {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?"
+    };
 
-//     public ListingActivity()
-//     {
+    private int listUserCount;
 
-//     }
+    public ListingActivity(string name, string description, int duration) : base(name, description, duration)
+    {
+        ;
+    }
 
-//     public void Run()
-//     {
 
-//     }
 
-//     public void GetRandomPrompt()
-//     {
+    public List<string> GetPrompt()
+    {
+        return _prompts;
+    }
 
-//     }
+    public int GetCount()
+    {
+        return _count = listUserCount;
+    }
 
-//     public List<string> GetListFromUser()
-//     {
-//         // It's very possible that I am going to change
-//         // this method later
-//         return new List<string>();
-//     }
+    public void Run()
+    {
+        Console.WriteLine("List as many responses you can to the following prompt:");
+        GetRandomPrompt();
+        Console.Write("You may begin in: ");
+        base.ShowCountDown(5);
+        Console.WriteLine();
 
-// }
+
+        List<string> entries = GetListFromUser();
+        listUserCount = entries.Count;
+
+        Console.WriteLine($"You listed {GetCount()} items!");
+
+    }
+
+    Random randomP = new Random();
+
+    public void GetRandomPrompt()
+    {
+        int randomIndex = randomP.Next(GetPrompt().Count);
+        string randomPrompt = GetPrompt()[randomIndex];
+        Console.WriteLine($" --- {randomPrompt} --- ");
+    }
+
+
+    public List<string> GetListFromUser()
+    {
+        // It's very possible that I am going to change
+        // this method later
+        List<string> Entries = new List<string>();
+        DateTime endTime = DateTime.Now.AddSeconds(base._duration);
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("> ");
+            string userEntry = Console.ReadLine();
+            Entries.Add(userEntry);
+        }
+
+        return Entries;
+    }
+
+}
--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -6,10 +6,40 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Mindfulness Project.");
 
-        BreathingActivity Ba1 = new BreathingActivity("new", "new", 30);
-        Ba1.Run();
+        int userChoice = 0;
 
-        ReflectingActivity Ba2 = new ReflectingActivity("new", "new", 30);
-        Ba2.Run();
+        while (userChoice != 4)
+        {
+            Console.WriteLine("Menu Options: ");
+            Console.WriteLine("  1. Start breathing activity");
+            Console.WriteLine("  2. Start reflecting activity");
+            Console.WriteLine("  3. Start listing activity");
+            Console.WriteLine("  4. Quit");
+            Console.Write("Select a choice from the menu: ");
+
+            userChoice = int.Parse(Console.ReadLine());
+
+            if (userChoice == 1)
+            {
+                int duration = int.Parse(Console.ReadLine());
+                BreathingActivity Ba1 = new BreathingActivity("Breathing Activity", "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.", duration);
+                Ba1.Run();
+            }
+
+            else if (userChoice == 2)
+            {
+                int duration = int.Parse(Console.ReadLine());
+                ReflectingActivity Ba2 = new ReflectingActivity("Reflection Activity", "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.", duration);
+                Ba2.Run();
+            }
+
+            else if (userChoice == 3)
+            {
+                int duration = int.Parse(Console.ReadLine());
+                ListingActivity Ba3 = new ListingActivity("Listing Activity", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.", duration);
+                Ba3.Run();
+            }
+        }
+
     }
 }

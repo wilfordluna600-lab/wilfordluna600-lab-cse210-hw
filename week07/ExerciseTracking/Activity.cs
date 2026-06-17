bc88bba7238a708f255a20f3c43e66f30fac98c2
wilfordluna600-lab <wilfordluna600@gmail.com>
2026-06-17 15:35:55 -0500

    changes

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -1,7 +1,30 @@
 public abstract class Activity
 {
+    private string _name;
     private float _distance;
     private float _minutes;
+
+    public Activity(string name, float distance, float minutes)
+    {
+        _name = name;
+        _distance = distance;
+        _minutes = minutes;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public float GetDistanceMeters()
+    {
+        return _distance;
+    }
+
+    public float GetMinutes()
+    {
+        return _minutes;
+    }
 
     public abstract float GetDistance();
 
@@ -9,5 +32,9 @@ public abstract class Activity
 
     public abstract float GetPace();
 
-    public abstract float GetSummary();
+    public string GetSummary()
+    {
+        DateTime now = DateTime.Now;
+        return $"{now.Day} {now.ToString("MMM")} {now.Year} {GetName()} ({GetMinutes()} min)- Distance: {GetDistance()} km, Speed: {GetSpeed()} kph, Pace: {GetPace()} min per km";
+    }
 }

137a7c16e3859c9dcb79572655661ef7c4b69b35
wilfordluna600-lab <wilfordluna600@gmail.com>
2026-06-17 16:43:30 -0500

    changes

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -6,6 +6,10 @@ class Program
     {
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
 
+        RunningActivity a1 = new RunningActivity("Running", 3600, 30);
 
+        CyclingActivity a2 = new CyclingActivity("Cycling", 5000, 30);
+
+        SwimmingActivity a3 = new SwimmingActivity("Swimming", 30, 25);
     }
 }

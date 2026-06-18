5824116a2e1e1ea1aeb85467ac373ef0ad5df4e1
wilfordluna600-lab <wilfordluna600@gmail.com>
2026-06-18 11:21:30 -0500

    changes

--- week06/EternalQuest/ChecklistGoal.cs
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -3,4 +3,24 @@ public class ChecklistGoal : Goal
     private int _amountCompleted;
     private int _target;
     private int _bonus;
+
+    public void RecordEvent()
+    {
+
+    }
+
+    public bool IsComplete()
+    {
+
+    }
+
+    public string GetDetailsString()
+    {
+
+    }
+
+    public string GetStringRepresentation()
+    {
+
+    }
 }
---
+++ week06/EternalQuest/GoalManager.cs
@@ -0,0 +1,45 @@
+public class GoalManager
+{
+    private List<Goal> _goals;
+    private int _score;
+
+    public void Start()
+    {
+
+    }
+
+    public void DisplayPlayerInfo()
+    {
+
+    }
+
+    public void ListGoalNames()
+    {
+
+    }
+
+    public void ListGoalDetails()
+    {
+
+    }
+
+    public void CreateGoal()
+    {
+
+    }
+
+    public void RecordEvent()
+    {
+
+    }
+
+    public void SaveGoals()
+    {
+
+    }
+
+    public void LoadGoals()
+    {
+
+    }
+}
